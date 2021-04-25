fc77dc2b3dcccd94e6ed4eb7371440de6c40b2fe
Slawek <s.radzyminski@ocado.com>
2021-04-25 18:37:20 +0200

    enable cors in spring security

--- src/main/java/com/awesome/testing/security/WebSecurityConfig.java
+++ src/main/java/com/awesome/testing/security/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.cors();
         http.csrf().disable();
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
