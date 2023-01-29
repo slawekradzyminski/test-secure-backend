909197c998568d0d98cd764bdb0242ce11ee6452
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-01-29 16:33:40 +0100

    Added custom script which verifies that the server is running

---
+++ waitForApp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Start the jar application
+java -jar ./jwt-auth-service-1.0.0.jar &
+
+# Counter for the waiting time
+counter=0
+
+# Wait for the port 4001 to be up, with a timeout of 180 seconds
+while ! nc -z localhost 4001; do
+  sleep 1
+  ((counter++))
+  if [ $counter -gt 180 ]; then
+    echo "Application failed to start after 120 seconds, exiting."
+    exit 1
+  fi
+  echo "Waiting for application to start... ($counter seconds)"
+done
