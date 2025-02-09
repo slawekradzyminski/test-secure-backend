dba5e3ccb3079a7a99a8d57b94f7c9393b0fa48d
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-02-09 17:44:08 +0100

    products refactor

--- src/test/java/com/awesome/testing/factory/ProductFactory.java
+++ src/test/java/com/awesome/testing/factory/ProductFactory.java
@@ -6,38 +6,44 @@ import com.awesome.testing.model.ProductEntity;
 import lombok.experimental.UtilityClass;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @UtilityClass
-public class ProductFactory {
+public class ProductFactory extends FakerSingleton {
 
     public static ProductEntity getRandomProduct() {
         return ProductEntity.builder()
-                .name("Test Product")
-                .description("Test Description")
-                .price(BigDecimal.valueOf(99.99))
-                .stockQuantity(10)
-                .category("Test Category")
+                .name(FAKER.commerce().productName())
+                .description(FAKER.lorem().sentence())
+                .price(generateValidPrice())
+                .stockQuantity(FAKER.random().nextInt(1, 100))
+                .category(FAKER.commerce().department())
                 .build();
     }
 
     public static ProductCreateDto getRandomProductCreate() {
         return ProductCreateDto.builder()
-                .name("Test Product")
-                .description("Test Description")
-                .price(BigDecimal.valueOf(99.99))
-                .stockQuantity(10)
-                .category("Test Category")
+                .name(FAKER.commerce().productName())
+                .description(FAKER.lorem().sentence())
+                .price(generateValidPrice())
+                .stockQuantity(FAKER.random().nextInt(1, 100))
+                .category(FAKER.commerce().department())
                 .build();
     }
 
     public static ProductUpdateDto getRandomProductUpdate() {
         return ProductUpdateDto.builder()
-                .name("Test Product")
-                .description("Test Description")
-                .price(BigDecimal.valueOf(99.99))
-                .stockQuantity(10)
-                .category("Test Category")
+                .name(FAKER.commerce().productName())
+                .description(FAKER.lorem().sentence())
+                .price(generateValidPrice())
+                .stockQuantity(FAKER.random().nextInt(1, 100))
+                .category(FAKER.commerce().department())
                 .build();
+    }
+
+    private static BigDecimal generateValidPrice() {
+        String priceStr = FAKER.commerce().price(0.01, 999999.99);
+        return new BigDecimal(priceStr).setScale(2, RoundingMode.HALF_UP);
     }
 
 }
